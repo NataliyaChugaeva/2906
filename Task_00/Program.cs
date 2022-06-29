378ec5f1e854ab9146aa7f55d39ddb2f95eb973b
Nataliya <nat.georgieva2017@yandex.ru>
2022-06-29 21:37:00 +0300

    Создала программу

--- Task_00/Program.cs
+++ Task_00/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//Console.Write ("Введите целое число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int square = num * num ;
+
+double d = 5.55;
+int i = (int)d;
+Console.WriteLine(i);
+//Console.Write ($"Квадрат числа {num} = {square}");
+
+
